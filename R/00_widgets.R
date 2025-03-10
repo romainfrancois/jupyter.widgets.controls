306c0ec801134a6551251a7368974cd1ccaeb0a1
Romain Francois <romain@tada.science>
2025-03-10 09:51:48 +0100

    +CoreWidget and DOMWidget

--- R/00_widgets.R
+++ R/00_widgets.R
@@ -30,6 +30,53 @@ jupyter.widget.Widget <- R6Class("jupyter.widget.Widget",
   )
 )
 
+jupyter.widget.CoreWidget <- R6Class("jupyter.widget.CoreWidget",
+  inherit = jupyter.widget.Widget,
+
+  public = list(
+    initialize = function(..., error_call = caller_env()) {
+      super$initialize(..., error_call = error_call)
+    }
+  )
+)
+
+read_only_state <- function(name) {
+  function() {
+    private$state[[name]]
+  }
+}
+
+jupyter.widget.DOMWidget <- R6Class("jupyter.widget.DOMWidget",
+  inherit = jupyter.widget.CoreWidget,
+
+  public = list(
+    initialize = function(layout = Layout(), model_name, dom_classes, tabbable, tooltip, ..., error_call = caller_env()) {
+      private$layout <- layout
+      private$state[c("_model_name", "_dom_classes", "tabbable", "tooltip")] <- list(model_name, dom_classes, tabbable, tooltip)
+      super$initialize(..., error_call = error_call)
+    }
+  ),
+
+  active = list(
+    model_name = private$read_only_state("_model_name"),
+    dom_classes = read_only_state("_dom_classes"),
+    tabbable = read_only_state("tabbable"),
+    tooltip = read_only_state("tooltip")
+  ),
+
+  private = list(
+    layout = NULL,
+
+    state = list(
+      "_model_name"  = character(),
+      "_dom_classes" = character(),
+      tabbable       = logical(),
+      tooltip        = character()
+    )
+  )
+)
+
+
 #' @importFrom hera mime_types
 #' @export
 mime_types.jupyter.widget.Widget <- function(x) {
