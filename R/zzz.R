fba69e13c006c2483957afda94b80c3d734757e6
Romain Francois <romain@tada.science>
2025-03-14 15:46:27 +0100

    caller_arg()

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 #' @import glue
 #' @import assertthat
 #' @importFrom purrr map_chr walk
-#' @importFrom rlang current_env check_dots_empty caller_env arg_match is_true list2
+#' @importFrom rlang current_env check_dots_empty caller_env arg_match is_true list2 caller_arg
 #' @importFrom fontawesome fa_metadata
 #' @importFrom cli cli_abort
 NULL
