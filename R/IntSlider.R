dfb788179c1abf72e5d7ca62560b9f1849fce58b
Romain Francois <romain@tada.science>
2025-03-25 16:51:02 +0100

    generate Box

--- R/IntSlider.R
+++ R/IntSlider.R
@@ -1,5 +1,3 @@
 #' @include IntSlider-generated.R
 #' @include accepted.R
 NULL
-
-set_widget_state_check(jupyter.widget.IntSlider, "orientation", jupyter.widgets.base::unbox_one_of(accepted_orientation))
