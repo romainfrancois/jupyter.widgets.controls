f412d1cb74f9d51a8b19fc485bb2d73a764ec78d
Romain Francois <romain@tada.science>
2025-03-25 14:12:34 +0100

    ButtonStyle-generated

--- data-raw/generated.R
+++ data-raw/generated.R
@@ -1,4 +1,6 @@
-library(jupyter.widgets.generate)
+library(jupyter.widgets.generate) # >= 0.0.0.9001
+
+writeLines(generate_style_widget("ButtonStyle"), "R/ButtonStyle-generated.R")
 
 writeLines(generate_dom_widget("Button", style = "ButtonStyle"), "R/Button-generated.R")
 writeLines(generate_dom_widget("IntSlider", style = "IntSliderStyle"), "R/IntSlider-generated.R")
