bbdedc48f35f0ded39e8bb1ad47df88d0fc8ae24
Romain Francois <romain@tada.science>
2025-03-26 16:31:36 +0100

    BoundedIntText widget

--- data-raw/generated.R
+++ data-raw/generated.R
@@ -7,6 +7,7 @@ writeLines(generate_style_widget("ButtonStyle"), "R/generated-ButtonStyle.R")
 # DOM Widgets
 writeLines(generate_dom_widget("Accordion", style = NULL), "R/generated-Accordion.R")
 writeLines(generate_dom_widget("BoundedFloatText", style = NULL), "R/generated-BoundedFloatText.R")
+writeLines(generate_dom_widget("BoundedIntText", style = NULL), "R/generated-BoundedIntText.R")
 writeLines(generate_dom_widget("Box", style = NULL), "R/generated-Box.R")
 writeLines(generate_dom_widget("Button", style = "ButtonStyle"), "R/generated-Button.R")
 writeLines(generate_dom_widget("IntSlider", style = "SliderStyle"), "R/generated-IntSlider.R")
