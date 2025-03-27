a8f9fb6fbd678d5f8cb7dd1a85fc150a8ace0360
Romain Francois <romain@tada.science>
2025-03-27 08:10:31 +0100

    Checkbox() + CheckboxStyle()

--- data-raw/generated.R
+++ data-raw/generated.R
@@ -3,11 +3,14 @@ library(jupyter.widgets.generate) # >= 0.0.0.9003
 # Style widgets
 writeLines(generate_style_widget("SliderStyle"), "R/generated-SliderStyle.R")
 writeLines(generate_style_widget("ButtonStyle"), "R/generated-ButtonStyle.R")
+writeLines(generate_style_widget("CheckboxStyle"), "R/generated-CheckboxStyle.R")
 
 # DOM Widgets
 writeLines(generate_dom_widget("Accordion", style = NULL), "R/generated-Accordion.R")
 writeLines(generate_dom_widget("BoundedFloatText", style = NULL), "R/generated-BoundedFloatText.R")
 writeLines(generate_dom_widget("BoundedIntText", style = NULL), "R/generated-BoundedIntText.R")
+writeLines(generate_dom_widget("Checkbox", style = "CheckboxStyle"), "R/generated-Checkbox.R")
+
 writeLines(generate_dom_widget("Box", style = NULL), "R/generated-Box.R")
 writeLines(generate_dom_widget("Button", style = "ButtonStyle"), "R/generated-Button.R")
 writeLines(generate_dom_widget("IntSlider", style = "SliderStyle"), "R/generated-IntSlider.R")
