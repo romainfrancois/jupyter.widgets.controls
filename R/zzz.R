5baccf54d767516aece0d12ae2a31f9218711470
Romain Francois <romain@tada.science>
2025-04-02 16:09:10 +0200

    ToggleButton/icon

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,8 @@ NULL
 .onLoad <- function(libname, pkgname) {
   rlang::run_on_load()
 
-  set_widget_state_check("jupyter.widget.Button", "icon"        , unbox_one_of(fa_metadata()$icon_names, allow_empty = TRUE))
+  set_widget_state_check("jupyter.widget.Button"      , "icon"  , unbox_one_of(fa_metadata()$icon_names, allow_empty = TRUE))
+  set_widget_state_check("jupyter.widget.ToggleButton", "icon"  , unbox_one_of(fa_metadata()$icon_names, allow_empty = TRUE))
 
   set_widget_state_check("jupyter.widget.FloatRangeSlider", "value", check_state_range_slider_value)
   set_widget_state_check("jupyter.widget.IntRangeSlider"  , "value", check_state_range_slider_value)
