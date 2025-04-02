e273160822807404b1862b632d4c19c08cb969a4
Romain Francois <romain@tada.science>
2025-04-02 13:22:10 +0200

    less generated files

--- R/wiipoc.R
+++ R/wiipoc.R
@@ -1,45 +1,5 @@
-#' @include generated-Accordion.R
-#' @include generated-BoundedFloatText.R
-#' @include generated-BoundedIntText.R
-#' @include generated-Box.R
-#' @include generated-VBox.R
-#' @include generated-HBox.R
-#' @include generated-GridBox.R
-#' @include generated-Tab.R
-#' @include generated-Stack.R
-#' @include generated-Button.R
-#' @include generated-Checkbox.R
-#' @include generated-CheckboxStyle.R
-#' @include generated-ColorsInput.R
-#' @include generated-Combobox.R
-#' @include generated-DatePicker.R
-#' @include generated-Datetime.R
-#' @include generated-NaiveDatetime.R
-#' @include generated-IntSlider.R
-#' @include generated-FloatSlider.R
-#' @include generated-FloatLogSlider.R
-#' @include generated-FloatRangeSlider.R
-#' @include generated-FloatRangeSlider.R
-#' @include generated-Text.R
-#' @include generated-IntText.R
-#' @include generated-FloatText.R
-#' @include generated-BoundedIntText.R
-#' @include generated-BoundedFloatText.R
-#' @include generated-HTML.R
-#' @include generated-HTMLMath.R
-#' @include generated-ToggleButton.R
-#' @include generated-IntProgress.R
-#' @include generated-FloatProgress.R
-#' @include generated-Password.R
-#' @include generated-Textarea.R
-#' @include generated-Time.R
-#' @include tools.R
-NULL
-
-#' @include generated-ButtonStyle.R
-#' @include generated-CheckboxStyle.R
-#' @include generated-SliderStyle.R
-#' @include generated-TextStyle.R
+#' @include generated-DOM.R
+#' @include generated-Style.R
 NULL
 
 jupyter.widget.Button$set("private", "before_comm_open", function() {
@@ -70,7 +30,6 @@ Stack              <- reformals(Stack, "children")
 
 Accordion          <- reformals(Accordion, "children")
 BoundedFloatText   <- reformals(BoundedFloatText, c("min", "max", "value", "step"))
-BoundedIntText     <- reformals(BoundedIntText, c("min", "max", "value", "step"))
 Combobox           <- reformals(Combobox, c("value"))
 DatePicker         <- reformals(DatePicker, c("value", "min", "max", "step"))
 Datetime           <- reformals(Datetime, c("value", "min", "max"))
