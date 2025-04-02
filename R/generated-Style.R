419914c30902d44029359a9c9e708a94324b1f78
Romain Francois <romain@tada.science>
2025-04-02 15:56:59 +0200

    and font_style

--- R/generated-Style.R
+++ R/generated-Style.R
@@ -214,6 +214,7 @@ ButtonStyle <- function(
 rlang::on_load({
   set_widget_state_check("jupyter.widget.ButtonStyle", "font_variant", unbox_one_of(c("normal", "small-caps", "all-small-caps", "petite-caps", "all-petite-caps", "unicase", "titling-caps"), allow_null = TRUE, allow_empty = FALSE))
   set_widget_state_check("jupyter.widget.ButtonStyle", "text_decoration", unbox_one_of(c("none", "underline", "overline", "line-through", "blink"), allow_null = TRUE, allow_empty = FALSE))
+  set_widget_state_check("jupyter.widget.ButtonStyle", "font_style", unbox_one_of(c("normal", "italic", "oblique"), allow_null = TRUE, allow_empty = FALSE))
 })
 
 #' CheckboxStyle widget
@@ -728,6 +729,7 @@ LabelStyle <- function(
 rlang::on_load({
   set_widget_state_check("jupyter.widget.LabelStyle", "font_variant", unbox_one_of(c("normal", "small-caps", "all-small-caps", "petite-caps", "all-petite-caps", "unicase", "titling-caps"), allow_null = TRUE, allow_empty = FALSE))
   set_widget_state_check("jupyter.widget.LabelStyle", "text_decoration", unbox_one_of(c("none", "underline", "overline", "line-through", "blink"), allow_null = TRUE, allow_empty = FALSE))
+  set_widget_state_check("jupyter.widget.LabelStyle", "font_style", unbox_one_of(c("normal", "italic", "oblique"), allow_null = TRUE, allow_empty = FALSE))
 })
 
 #' ProgressStyle widget
@@ -944,5 +946,6 @@ ToggleButtonStyle <- function(
 rlang::on_load({
   set_widget_state_check("jupyter.widget.ToggleButtonStyle", "font_variant", unbox_one_of(c("normal", "small-caps", "all-small-caps", "petite-caps", "all-petite-caps", "unicase", "titling-caps"), allow_null = TRUE, allow_empty = FALSE))
   set_widget_state_check("jupyter.widget.ToggleButtonStyle", "text_decoration", unbox_one_of(c("none", "underline", "overline", "line-through", "blink"), allow_null = TRUE, allow_empty = FALSE))
+  set_widget_state_check("jupyter.widget.ToggleButtonStyle", "font_style", unbox_one_of(c("normal", "italic", "oblique"), allow_null = TRUE, allow_empty = FALSE))
 })
 
--- R/zzz.R
+++ R/zzz.R
@@ -9,14 +9,11 @@
 #' @importFrom jsonlite unbox
 NULL
 
-accepted_font_style <- c("normal", "italic", "oblique")
-
 .onLoad <- function(libname, pkgname) {
   rlang::run_on_load()
 
   set_widget_state_check("jupyter.widget.Button", "icon"        , unbox_one_of(fa_metadata()$icon_names, allow_empty = TRUE))
 
-  set_widget_state_check("jupyter.widget.ButtonStyle", "font_style", unbox_one_of(accepted_font_style, allow_null = TRUE))
   set_widget_state_check("jupyter.widget.ButtonStyle", "font_weight", check_state_font_weight)
 
   set_widget_state_check("jupyter.widget.FloatRangeSlider", "value", check_state_range_slider_value)
